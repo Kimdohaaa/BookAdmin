a3b62edf80eeb8adec7f9637058708b3b5c35509
Kimdohaaa <jjeenee0408@gmail.com>
2025-02-10 10:52:35 +0900

    ---

---
+++ BookAdmin/src/main/java/model/dto/BookDto.java
@@ -0,0 +1,19 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class BookDto {
+	private int bno;
+	private String bname;
+	private String bwriter;
+	private String bcompany;
+	private String indate;
+	private String update;
+}
